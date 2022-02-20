4c4c9e8baf70777ee1ef4eb33bdd2e7a5cfeff45
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-02-20 17:08:36 +0100

    fix: moved LicenseManager And Collector in a folder

--- djangoPolyglot/management/commands/django-polyglot.py
+++ djangoPolyglot/management/commands/django-polyglot.py
@@ -4,7 +4,7 @@ from django.core import management
 from django.conf import settings
 
 from polyglot import polyglot, arguments
-from . import arguments as django_arguments
+from .arguments import arguments as django_arguments
 
 
 class Command(BaseCommand):
